b0c7f554f11a38201538143a2f7b6a7db0f3033c
BakPeter <bak.pet@gmail.com>
2021-08-17 11:40:47 +0300

    recipes component and dependecies htmls implemented

--- course-project/recipes-prj/src/app/recipes/recipe-list/recipe-list.component.ts
+++ course-project/recipes-prj/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A test recipe 1',
+      'This is a simple test 1',
+      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
+    ),
+    new Recipe(
+      'A test recipe 2',
+      'This is a simple test 2',
+      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
+    ),
+    new Recipe(
+      'A test recipe 3',
+      'This is a simple test 3',
+      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
+    ),
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
